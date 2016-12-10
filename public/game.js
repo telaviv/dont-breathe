433c142e7163b69858f372bb8dad13b726b90a38
Shawn Krisman <krisman.shawn@gmail.com>
2016-12-10 11:31:51 -0800

    added a rectangle.

--- public/game.js
+++ public/game.js
@@ -1,14 +1,20 @@
-console.log(PIXI);
-console.log('sup');
-
 //Create the renderer
-var renderer = PIXI.autoDetectRenderer(256, 256);
+var renderer = PIXI.autoDetectRenderer(1024, 768);
 
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
 
 //Create a container object called the `stage`
 var stage = new PIXI.Container();
+
+var rectangle = new PIXI.Graphics();
+rectangle.lineStyle(4, 0xFF3300, 1);
+rectangle.beginFill(0x66CCFF);
+rectangle.drawRect(0, 0, 64, 64);
+rectangle.endFill();
+rectangle.x = 170;
+rectangle.y = 170;
+stage.addChild(rectangle);
 
 //Tell the `renderer` to `render` the `stage`
 renderer.render(stage);
