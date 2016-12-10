368c5f22f3ea88caeea39042caf5931963434187
Shawn Krisman <krisman.shawn@gmail.com>
2016-12-10 12:09:57 -0800

    Created the GameScene.

--- public/game.js
+++ public/game.js
@@ -1,6 +1,6 @@
 class O2Meter {
   draw() {
-    var rectangle = new PIXI.Graphics();
+    const rectangle = new PIXI.Graphics();
     rectangle.lineStyle(4, 0xFF3300, 1);
     rectangle.beginFill(0x66CCFF);
     rectangle.drawRect(0, 0, 64, 64 * 6);
@@ -9,26 +9,28 @@ class O2Meter {
   }
 }
 
+class GameScene {
+  constructor() {
+    this.o2meter = new O2Meter();
+  }
+
+  draw() {
+    const stage = new PIXI.Container();
+    const o2sprite = this.o2meter.draw();
+    o2sprite.x = 170;
+    o2sprite.y = 170;
+    stage.addChild(o2sprite);
+    return stage;
+  }
+}
+
+
+
 //Create the renderer
 var renderer = PIXI.autoDetectRenderer(1024, 768);
 
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
 
-//Create a container object called the `stage`
-var stage = new PIXI.Container();
-var o2Meter = (new O2Meter()).draw();
-o2Meter.x = 170;
-o2Meter.y = 170;
-stage.addChild(o2Meter);
-
-var message = new PIXI.Text(
-  "Hello Pixi!",
-  {font: "32px sans-serif", fill: "white"}
-);
-
-message.position.set(54, 96);
-stage.addChild(message);
-
 //Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
+renderer.render((new GameScene).draw());
